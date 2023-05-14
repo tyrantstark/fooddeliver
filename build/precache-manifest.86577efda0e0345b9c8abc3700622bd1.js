1a4f32d9285c0720a09d7c2ebf9f51df58188187
Abhishek kumar <ak749258@gmail.com>
2023-05-14 21:16:05 +0530

    first commit

---
+++ build/precache-manifest.86577efda0e0345b9c8abc3700622bd1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ca991c10e4a88f390b8c967b0a4901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "453059e320383ce86296",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "13093a22bcd7297ae11b",
+    "url": "/static/css/main.2be03680.chunk.css"
+  },
+  {
+    "revision": "453059e320383ce86296",
+    "url": "/static/js/2.aa6a51e6.chunk.js"
+  },
+  {
+    "revision": "2a0b20e1d02e77e8b582ff21a215748a",
+    "url": "/static/js/2.aa6a51e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13093a22bcd7297ae11b",
+    "url": "/static/js/main.6fdb3e9b.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  },
+  {
+    "revision": "90dabe9344551a1f7385104e2454b1e9",
+    "url": "/static/media/404.90dabe93.jpg"
+  },
+  {
+    "revision": "4c4dc7719477c9c6d48ba14c5d04af53",
+    "url": "/static/media/Mihir.4c4dc771.jpeg"
+  },
+  {
+    "revision": "42d709e436404ca5c9cad3c6de0c31d6",
+    "url": "/static/media/check.42d709e4.png"
+  },
+  {
+    "revision": "837e0b22921e25f5a2eca9d514a4a2e1",
+    "url": "/static/media/close.837e0b22.png"
+  },
+  {
+    "revision": "e295470bc88c8d972e83f4e3d77f7ea9",
+    "url": "/static/media/empty.e295470b.png"
+  },
+  {
+    "revision": "6293f16266f118ba96ae833d911701db",
+    "url": "/static/media/logo.6293f162.svg"
+  },
+  {
+    "revision": "a8f6abbb4ea76be277e34cbb17ca9175",
+    "url": "/static/media/noItem.a8f6abbb.png"
+  },
+  {
+    "revision": "bc72b72ec06fc6435aad6be9949ed8df",
+    "url": "/static/media/register.bc72b72e.svg"
+  }
+]);
